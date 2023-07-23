539cc8b9bff5a6d56704ca295a6f53b9b0f8c5ba
NikitaSiulin <n.siulin@rufago.ru>
2023-07-24 01:06:48 +0700

    sql ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+from datetime import timedelta, datetime
 from sqlalchemy import (
     create_engine,
     MetaData,
@@ -9,11 +9,13 @@ from sqlalchemy import (
     ForeignKey,
     Date,
     Float,
-    Time,
+    Interval,
 )
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine("sqlite:///db.sqlite3")
-session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 meta = MetaData()
 
 
@@ -21,9 +23,9 @@ class Base(DeclarativeBase):
     pass
 
 
-class Data(Base):
+class Data:
     decimal = Column(Float)
-    digital = Column(Time)
+    digital = Column(Interval)
     hours = Column(Integer)
     minutes = Column(Integer)
     percent = Column(Float)
@@ -32,37 +34,38 @@ class Data(Base):
     total_seconds = Column(Float)
 
 
-class Categories(Data):
+class Categories(Base, Data):
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
 
 
-class Editors(Data):
+class Editors(Base, Data):
     __tablename__ = "editors"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
 
 
-class Machines(Data):
+class Machines(Base, Data):
     __tablename__ = "machines"
     id = Column(Integer, primary_key=True, index=True)
+    machine_name_id = Column(String)
     name = Column(String)
 
 
-class Languages(Data):
+class Languages(Base, Data):
     __tablename__ = "languages"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
 
 
-class OperatingSystems(Data):
+class OperatingSystems(Base, Data):
     __tablename__ = "operating_systems"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
 
 
-class Projects(Data):
+class Projects(Base, Data):
     __tablename__ = "projects"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
@@ -73,30 +76,36 @@ class Days(Base):
     id = Column(Integer, primary_key=True, index=True)
     date = Column(Date)
     categories_id = Column(
-        Integer, ForeignKey("categories.id", ondelete="SET_NULL"), nullable=True
+        Integer, ForeignKey("categories.id", ondelete="SET NULL"), nullable=True
     )
     projects_id = Column(
-        Integer, ForeignKey("projects.id", ondelete="SET_NULL"), nullable=True
+        Integer, ForeignKey("projects.id", ondelete="SET NULL"), nullable=True
     )
     operating_systems_id = Column(
-        Integer, ForeignKey("operating_systems.id", ondelete="SET_NULL"), nullable=True
+        Integer, ForeignKey("operating_systems.id", ondelete="SET NULL"), nullable=True
     )
     languages_id = Column(
-        Integer, ForeignKey("languages.id", ondelete="SET_NULL"), nullable=True
+        Integer, ForeignKey("languages.id", ondelete="SET NULL"), nullable=True
     )
     machines_id = Column(
-        Integer, ForeignKey("machines.id", ondelete="SET_NULL"), nullable=True
+        Integer, ForeignKey("machines.id", ondelete="SET NULL"), nullable=True
     )
     editors_id = Column(
-        Integer, ForeignKey("editors.id", ondelete="SET_NULL"), nullable=True
+        Integer, ForeignKey("editors.id", ondelete="SET NULL"), nullable=True
     )
 
 
-def create_generic_table(data: list, day: dict, name: str, table: Base):
+def create_generic_table(day_data: list, day: dict, name: str, table: Base):
     table_data = day.get(name, {})
     if table_data:
         for item in table_data:
-            data.append(table(**item))
+            time = datetime.strptime(item["digital"], "%H:%M:%S")
+            item["digital"] = timedelta(
+                hours=time.hour, minutes=time.minute, seconds=time.second
+            )
+            row = table(**item)
+            day_data[f"{name}_id"] = row.id
+            session.add(row)
 
 
 def import_json_data(path: str):
@@ -107,18 +116,29 @@ def import_json_data(path: str):
         "languages": Languages,
         "operating_systems": OperatingSystems,
     }
-    json_data = json.load(path)["days"]
-    data = []
+    with open(path, "r", encoding="utf-8") as file:
+        json_data = json.load(file)["days"]
     for day in json_data:
+        day_data = {}
         for data_type, table in data_types.items():
-            create_generic_table(data, day, data_type, table)
+            create_generic_table(day_data, day, data_type, table)
         projects = day.get("projects", {})
         if projects:
             for project in projects:
-                data.append(
-                    Projects(
-                        name=project["name"],
-                        **project["grand_total"],
-                    )
+                project_time = project["grand_total"]["digital"]
+                time = datetime.strptime(project_time, "%H:%M").time()
+                project["grand_total"]["digital"] = timedelta(
+                    hours=time.hour, minutes=time.minute, seconds=time.second
                 )
-    meta.create_all(engine)
+                project_row = Projects(
+                    name=project["name"],
+                    **project["grand_total"],
+                )
+                day_data["projects_id"] = project_row.id
+                session.add(project_row)
+        day_row = Days(date=datetime.strptime(day["date"], "%Y-%m-%d").date(), **day_data)
+        session.add(day_row)
+    session.commit()
+
+
+Base.metadata.create_all(engine)
